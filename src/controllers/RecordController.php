7bfdf8d4e4b8e47746491c443052b23a7c8c1de2
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2018-06-09 11:17:27 +0200

    batch code update - app-base

--- src/controllers/RecordController.php
+++ src/controllers/RecordController.php
@@ -35,6 +35,14 @@ class RecordController extends MainController
         return self::create()->Link( $action );
     }
     
+    public static function Guid()
+    {
+        mt_srand((double)microtime() * 10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        return substr($charid, 0, 8) . $hyphen . substr($charid, 8, 4) . $hyphen . substr($charid, 12, 4) . $hyphen . substr($charid, 16, 4) . $hyphen . substr($charid, 20, 12);
+    }
+    
     function generate_page_controller( $title = "Page" )
     {
         $tmpPage             = new \Page();
