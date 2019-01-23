b3015b2d71bedddb5ec6c80eed044f5149932809
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-01-23 21:23:22 +0200

    batch code update - app-base

--- _config.php
+++ _config.php
@@ -1,23 +1,23 @@
 <?php
-    /**
-     *
-     * @copyright (c) 2017 Insite Apps - http://www.insiteapps.co.za
-     * @package       insiteapps
-     * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
-     * All rights reserved. No warranty, explicit or implicit, provided.
-     *
-     * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if
-     * any.
-     * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and
-     * may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or
-     * copyright laws. Dissemination of this information or reproduction of this material is strictly forbidden unless
-     * prior written permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use,
-     * share or change this file.
-     *
-     *
-     */
+/**
+ *
+ * @copyright (c) 2017 Insite Apps - http://www.insiteapps.co.za
+ * @package       insiteapps
+ * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * All rights reserved. No warranty, explicit or implicit, provided.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if
+ * any.
+ * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and
+ * may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or
+ * copyright laws. Dissemination of this information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use,
+ * share or change this file.
+ *
+ *
+ */
 
-    define('COMMON_DIR', basename(dirname(__FILE__)));
-    define('APP_BASE_DIR', basename(__DIR__));
-    define('FONTS_DIR', COMMON_DIR . "/fonts");
+define( 'COMMON_DIR', basename( __DIR__ ) );
+define( 'APP_BASE_DIR', basename( __DIR__ ) );
+define( 'FONTS_DIR', COMMON_DIR . '/fonts' );
 
