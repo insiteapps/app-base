2ea46ebb668284339ef4ba0d28f311be103a4cae
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-02-11 10:07:21 +0200

    batch code update - app-base

--- src/Security/MemberAuthenticator/MembershipAuthenticator.php
+++ src/Security/MemberAuthenticator/MembershipAuthenticator.php
@@ -2,46 +2,47 @@
 /**
  *
  * @copyright (c) 2018 Insite Apps - http://www.insiteapps.co.za
- * @package insiteapps
- * @author Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * @package       insiteapps
+ * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
  * All rights reserved. No warranty, explicit or implicit, provided.
  *
- * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.  
- * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright laws.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Insite Apps.
- * Proprietary and confidential.
- * There is no freedom to use, share or change this file.
+ * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.
+ * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be
+ * covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ * laws. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use, share or change
+ * this file.
  *
  *
  */
 
-namespace InsiteApps\Security\MemberAuthenticator;
-
-
-use SilverStripe\Security\Authenticator;
-use SilverStripe\Security\MemberAuthenticator\MemberAuthenticator;
-
-/**
- * Provides authentication for the user within the CMS
- */
-class MembershipAuthenticator extends MemberAuthenticator
-{
+namespace InsiteApps\Security\MemberAuthenticator {
     
-    public function supportedServices()
-    {
-        // Bitwise-OR of all the supported services in this Authenticator, to make a bitmask
-        return Authenticator::LOGIN | Authenticator::LOGOUT | Authenticator::CHANGE_PASSWORD
-            | Authenticator::RESET_PASSWORD | Authenticator::CHECK_PASSWORD;
-    }
     
+    use SilverStripe\Security\Authenticator;
+    use SilverStripe\Security\MemberAuthenticator\MemberAuthenticator;
     
     /**
-     * @param string $link
-     *
-     * @return \SilverStripe\Security\MemberAuthenticator\LoginHandler|static
+     * Provides authentication for the user within the CMS
      */
-    public function getLoginHandler( $link )
+    class MembershipAuthenticator extends MemberAuthenticator
     {
-        return MembershipLoginHandler::create( $link, $this );
+        
+        public function supportedServices()
+        {
+            // Bitwise-OR of all the supported services in this Authenticator, to make a bitmask
+            return Authenticator::LOGIN | Authenticator::LOGOUT | Authenticator::CHANGE_PASSWORD | Authenticator::RESET_PASSWORD | Authenticator::CHECK_PASSWORD;
+        }
+        
+        
+        /**
+         * @param string $link
+         *
+         * @return \SilverStripe\Security\MemberAuthenticator\LoginHandler|static
+         */
+        public function getLoginHandler( $link )
+        {
+            return MembershipLoginHandler::create( $link, $this );
+        }
     }
 }
--- src/Security/SecurityExtension.php
+++ src/Security/SecurityExtension.php
@@ -2,40 +2,42 @@
 /**
  *
  * @copyright (c) 2018 Insite Apps - http://www.insiteapps.co.za
- * @package insiteapps
- * @author Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * @package       insiteapps
+ * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
  * All rights reserved. No warranty, explicit or implicit, provided.
  *
  * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.
- * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright laws.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Insite Apps.
- * Proprietary and confidential.
- * There is no freedom to use, share or change this file.
+ * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be
+ * covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ * laws. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use, share or change
+ * this file.
  *
  *
  */
 
-namespace InsiteApps\Security;
-
-use InsiteApps\Security\MemberAuthenticator\MembershipAuthenticator;
-use SilverStripe\Core\Injector\Injector;
-use SilverStripe\ORM\DataExtension;
-
-
-class SecurityExtension extends DataExtension
-{
+namespace InsiteApps\Security {
     
-    private static $allowed_actions = array(
-        'MembershipLoginForm',
-    );
+    use InsiteApps\Security\MemberAuthenticator\MembershipAuthenticator;
+    use SilverStripe\Core\Injector\Injector;
+    use SilverStripe\ORM\DataExtension;
     
-    /**
-     * @return mixed
-     * @throws \Psr\Container\NotFoundExceptionInterface
-     */
-    public function MembershipLoginForm()
+    
+    class SecurityExtension extends DataExtension
     {
-        return Injector::inst()->get( MembershipAuthenticator::class )->getLoginHandler( $this->owner->Link() )
-                       ->MembershipLoginForm();
+        
+        private static $allowed_actions = array(
+            'MembershipLoginForm',
+        );
+        
+        /**
+         * @return mixed
+         * @throws \Psr\Container\NotFoundExceptionInterface
+         */
+        public function MembershipLoginForm()
+        {
+            return Injector::inst()->get( MembershipAuthenticator::class )->getLoginHandler( $this->owner->Link() )
+                           ->MembershipLoginForm();
+        }
     }
 }
