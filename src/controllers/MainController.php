d88f028951a9e869b455445dd5da7c64ca3daa67
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-02-19 15:00:20 +0200

    batch code update - app-base

--- src/controllers/MainController.php
+++ src/controllers/MainController.php
@@ -141,16 +141,8 @@ class MainController extends Controller
     
     public static function RemoveProtocol( $url )
     {
-        $disallowed = array(
-            'http://',
-            'https://',
-        );
-        foreach ( $disallowed as $d ) {
-            if ( strpos( $url, $d ) === 0 ) {
-                return str_replace( $d, '', $url );
-            }
-        }
-        return $url;
+        return preg_replace( '(^https?://)', '', $url );
+        
     }
     
     /**
