f9cc00cab63f5c919269bd3b098dab1c60ea2a64
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-03-02 17:28:18 +0200

    batch code update - app-base

--- src/extensions/DataObjectURLSegmentExtension.php
+++ src/extensions/DataObjectURLSegmentExtension.php
@@ -2,15 +2,16 @@
 /**
  *
  * @copyright (c) 2018 Insite Apps - http://www.insiteapps.co.za
- * @package insiteapps
- * @author Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * @package       insiteapps
+ * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
  * All rights reserved. No warranty, explicit or implicit, provided.
  *
  * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.
- * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright laws.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Insite Apps.
- * Proprietary and confidential.
- * There is no freedom to use, share or change this file.
+ * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be
+ * covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ * laws. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use, share or change
+ * this file.
  *
  *
  */
@@ -122,12 +123,14 @@ class DataObjectURLSegmentExtension extends DataExtension
             $aChanged = array_intersect( array_keys( $aChangedFields ), $aFields );
             if ( count( $aChanged ) ) {
                 $this->owner->URLSegment = $this->generateUniqueURLSegment( $this->owner->Title );
+                $this->owner->URLSegment = $this->generateUniqueURLSegment( $this->owner->Title );
             }
         }
+        $name                    = $this->owner->Title ? : $this->owner->Name;
+        $this->owner->URLSegment = $this->generateUniqueURLSegment( $name );
+        //if ( !$this->owner->URLSegment ) {
         
-        if ( !$this->owner->URLSegment ) {
-            $this->owner->URLSegment = $this->generateUniqueURLSegment( $this->owner->Title );
-        }
+        //}
         
         parent::onBeforeWrite();
     }
