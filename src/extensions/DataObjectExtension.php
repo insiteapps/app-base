e2294cc4551737edeaf5a7f4eac6e9ed12eb87e2
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2019-03-11 15:34:25 +0200

    batch code update - app-base

--- src/extensions/DataObjectExtension.php
+++ src/extensions/DataObjectExtension.php
@@ -491,6 +491,21 @@ namespace InsiteApps\ORM {
             return "<img src=\"$url\" alt=\"No photo\" />";
         }
         
+        
+        public function getGUID(){
+            mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+            $charid = strtoupper(md5(uniqid(rand(), true)));
+            $hyphen = chr(45);// "-"
+            $uuid = chr(123)// "{"
+                .substr($charid, 0, 8).$hyphen
+                .substr($charid, 8, 4).$hyphen
+                .substr($charid,12, 4).$hyphen
+                .substr($charid,16, 4).$hyphen
+                .substr($charid,20,12)
+                .chr(125);// "}"
+            return $uuid;
+        }
+        
         public static function truncate( $text, $length = "50", $ending = '...', $exact = false, $considerHtml = true )
         {
             if ( $considerHtml ) {
