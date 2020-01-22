0cf50e13da8ef43c4664f74912b6c6d26a8777d5
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2020-01-22 13:39:51 +0200

    batch code update - app-base

--- src/Security/GroupLoginExtension.php
+++ src/Security/GroupLoginExtension.php
@@ -2,47 +2,49 @@
 /**
  *
  * @copyright (c) 2018 Insite Apps - http://www.insiteapps.co.za
- * @package insiteapps
- * @author Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * @package       insiteapps
+ * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
  * All rights reserved. No warranty, explicit or implicit, provided.
  *
- * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.  
- * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright laws.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Insite Apps.
- * Proprietary and confidential.
- * There is no freedom to use, share or change this file.
+ * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.
+ * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be
+ * covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ * laws. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use, share or change
+ * this file.
  *
  *
  */
 
-namespace InsiteApps\Security;
+namespace InsiteApps\Security {
 
-use SilverStripe\CMS\Model\SiteTree;
-use SilverStripe\Forms\CheckboxField;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TreeDropdownField;
-use SilverStripe\ORM\DataExtension;
+    use SilverStripe\CMS\Model\SiteTree;
+    use SilverStripe\Forms\CheckboxField;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\TreeDropdownField;
+    use SilverStripe\ORM\DataExtension;
 
 
-class GroupLoginExtension extends DataExtension
-{
-    
-    private static $db = array(
-        "GoToAdmin" => "Boolean",
-    );
-    
-    private static $has_one = array(
-        "LinkPage" => "SiteTree",
-    );
-    
-    public function updateCMSFields( FieldList $fields )
+    class GroupLoginExtension extends DataExtension
     {
-        
-        $fields->addFieldsToTab( "Root.Members(after login)", [
-            CheckboxField::create( "GoToAdmin", " Go to Admin area" ),
-            TreeDropdownField::create( "LinkPageID", "Or select a Page to redirect to", SiteTree::class ),
-        ] );
-        
+
+        private static $db = [
+            "GoToAdmin" => "Boolean",
+        ];
+
+        private static $has_one = [
+            'LinkPage' => SiteTree::class,
+        ];
+
+        public function updateCMSFields(FieldList $fields)
+        {
+
+            $fields->addFieldsToTab('Root.Members(after login)', [
+                CheckboxField::create('GoToAdmin','Go to Admin area'),
+                TreeDropdownField::create('LinkPageID', 'Or select a Page to redirect to', SiteTree::class),
+            ]);
+
+        }
+
     }
-    
 }
