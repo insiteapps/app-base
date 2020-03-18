05e4c34e8fb1c0c14d152c6ff5976b4912a03e91
Itayi Patrick Chito-voro <patrick@chitosystems.com>
2020-03-18 23:33:56 +0200

    batch code update - app-base

--- src/Security/MemberAuthenticator/MembershipLoginHandler.php
+++ src/Security/MemberAuthenticator/MembershipLoginHandler.php
@@ -16,68 +16,69 @@
  *
  */
 
-namespace InsiteApps\Security\MemberAuthenticator;
+namespace InsiteApps\Security\MemberAuthenticator {
 
-use SilverStripe\Control\Director;
-use SilverStripe\Control\HTTPResponse;
-use SilverStripe\Dev\Debug;
-use SilverStripe\Security\Group;
-use SilverStripe\Security\MemberAuthenticator\LoginHandler;
-use SilverStripe\Security\Security;
+    use SilverStripe\Control\Director;
+    use SilverStripe\Control\HTTPResponse;
+    use SilverStripe\Dev\Debug;
+    use SilverStripe\Security\Group;
+    use SilverStripe\Security\MemberAuthenticator\LoginHandler;
+    use SilverStripe\Security\Security;
 
-class MembershipLoginHandler extends LoginHandler
-{
-
-    private static $allowed_actions = [
-        'doLogin',
-    ];
-
-
-    /**
-     * Send user to the right location after login
-     *
-     * @return HTTPResponse
-     */
-    protected function redirectAfterSuccessfulLogin()
+    class MembershipLoginHandler extends LoginHandler
     {
 
-
-        // Check password expiry
-        if ( Security::getCurrentUser()->isPasswordExpired() ) {
-            // Redirect the user to the external password change form if necessary
-            return $this->redirectToChangePassword();
-        }
+        private static $allowed_actions = [
+            'doLogin',
+        ];
 
 
-        $oMember = Security::getCurrentUser();
+        /**
+         * Send user to the right location after login
+         *
+         * @return HTTPResponse
+         */
+        protected function redirectAfterSuccessfulLogin()
+        {
 
-        $oGroups = Group::get();
-        if ( $oMember ) {
-            foreach ( $oGroups as $oGroup ) {
-                if ( $oMember->inGroup($oGroup->ID) && $oGroup->GoToAdmin == 1 ) {
-                    return $this->redirect(Director::baseURL() . 'admin');
 
-                } elseif ( $oMember->inGroup($oGroup->ID) && $oGroup->LinkPageID != 0 ) {
-                    $pageLink = $oGroup->LinkPage()->Link();
+            // Check password expiry
+            if ( Security::getCurrentUser()->isPasswordExpired() ) {
+                // Redirect the user to the external password change form if necessary
+                return $this->redirectToChangePassword();
+            }
 
-                    return $this->redirect($pageLink);
 
+            $oMember = Security::getCurrentUser();
+
+            $oGroups = Group::get();
+            if ( $oMember ) {
+                foreach ( $oGroups as $oGroup ) {
+                    if ( $oMember->inGroup($oGroup->ID) && $oGroup->GoToAdmin == 1 ) {
+                        return $this->redirect(Director::baseURL() . 'admin');
+
+                    } elseif ( $oMember->inGroup($oGroup->ID) && $oGroup->LinkPageID != 0 ) {
+                        $pageLink = $oGroup->LinkPage()->Link();
+
+                        return $this->redirect($pageLink);
+
+                    }
                 }
             }
-        }
 
-        // Debug::show( $oMember );
+            // Debug::show( $oMember );
 
-        //   return;
+            //   return;
 
-        if ( isset($_REQUEST[ 'BackURL' ]) && $_REQUEST[ 'BackURL' ] && Director::is_site_url($_REQUEST[ 'BackURL' ]) ) {
-            $BackURL = $_REQUEST[ 'BackURL' ];
+            if ( isset($_REQUEST[ 'BackURL' ]) && $_REQUEST[ 'BackURL' ] && Director::is_site_url($_REQUEST[ 'BackURL' ]) ) {
+                $BackURL = $_REQUEST[ 'BackURL' ];
 
-            return $this->redirect($BackURL);
-        } else {
+                return $this->redirect($BackURL);
+            } else {
+                return $this->redirect('/');
+            }
+
             return $this->redirect('/');
         }
-
-        return $this->redirect('/');
     }
 }
