8847b3f47219c3169266ce78a8ebd143e1727026
Itayi Patrick Chito-voro <patrick@chitosystems.com>
2020-03-21 21:01:35 +0200

    batch code update - app-base

--- src/Managers/UtilityManager.php
+++ src/Managers/UtilityManager.php
@@ -2,15 +2,16 @@
 /**
  *
  * @copyright (c) 2018 Insite Apps - http://www.insiteapps.co.za
- * @package insiteapps
- * @author Patrick Chitovoro  <patrick@insiteapps.co.za>
+ * @package       insiteapps
+ * @author        Patrick Chitovoro  <patrick@insiteapps.co.za>
  * All rights reserved. No warranty, explicit or implicit, provided.
  *
  * NOTICE:  All information contained herein is, and remains the property of Insite Apps and its suppliers,  if any.
- * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright laws.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Insite Apps.
- * Proprietary and confidential.
- * There is no freedom to use, share or change this file.
+ * The intellectual and technical concepts contained herein are proprietary to Insite Apps and its suppliers and may be
+ * covered by South African. and Foreign Patents, patents in process, and are protected by trade secret or copyright
+ * laws. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Insite Apps. Proprietary and confidential. There is no freedom to use, share or change
+ * this file.
  *
  *
  */
@@ -26,44 +27,88 @@ use SilverStripe\Security\Security;
 class UtilityManager
 {
     use Injectable;
-    
+
     public function __construct()
     {
         parent::__construct();
-        
+
     }
-    
+
     public static function includeSpectrum()
     {
-        
+
         ///Requirements::javascript(SPECTRUM_DIR . '/js/spectrum.js');
         //Requirements::javascript(SPECTRUM_DIR . '/js/spectrum.int.js');
         //Requirements::css(SPECTRUM_DIR . '/css/spectrum.css');
         //Requirements::css(SPECTRUM_DIR . '/css/spectrum.custom.css');
-        
+
     }
-    
-    public static function camelCase( $str )
+
+
+    public static function parseURL($url, $retdata = true)
     {
-        $str = self::convertToLowercase( $str );
-        
-        return preg_replace_callback( '/_([a-z])/', function ( $match ) {
-            return strtoupper( $match[ 1 ] );
-        }, $str );
-    }
-    
-    
-    public static function SentenceCase($string) {
-        $sentences = preg_split('/([.?!]+)/', $string, -1,PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE);
-        $newString = '';
-        foreach ($sentences as $key => $sentence) {
-            $newString .= ($key & 1) === 0?
-                ucfirst(strtolower(trim($sentence))) :
-                $sentence.' ';
+        $url = substr($url, 0, 4) == 'http' ? $url : 'http://' . $url; //assume http if not supplied
+        if ( $urldata = parse_url(str_replace('&amp;', '&', $url)) ) {
+            $path_parts = pathinfo($urldata[ 'host' ]);
+            $tmp = explode('.', $urldata[ 'host' ]);
+            $n = count($tmp);
+            if ( $n >= 2 ) {
+                if ( $n == 4 || ( $n == 3 && strlen($tmp[ ( $n - 2 ) ]) <= 3 ) ) {
+                    $urldata[ 'domain' ] = $tmp[ ( $n - 3 ) ] . "." . $tmp[ ( $n - 2 ) ] . "." . $tmp[ ( $n - 1 ) ];
+                    $urldata[ 'tld' ] = $tmp[ ( $n - 2 ) ] . "." . $tmp[ ( $n - 1 ) ]; //top-level domain
+                    $urldata[ 'root' ] = $tmp[ ( $n - 3 ) ]; //second-level domain
+                    $urldata[ 'subdomain' ] = $n == 4 ? $tmp[ 0 ] : ( $n == 3 && strlen($tmp[ ( $n - 2 ) ]) <= 3 ) ? $tmp[ 0 ] : '';
+                } else {
+                    $urldata[ 'domain' ] = $tmp[ ( $n - 2 ) ] . "." . $tmp[ ( $n - 1 ) ];
+                    $urldata[ 'tld' ] = $tmp[ ( $n - 1 ) ];
+                    $urldata[ 'root' ] = $tmp[ ( $n - 2 ) ];
+                    $urldata[ 'subdomain' ] = $n == 3 ? $tmp[ 0 ] : '';
+                }
+            }
+            //$urldata['dirname'] = $path_parts['dirname'];
+            $urldata[ 'basename' ] = $path_parts[ 'basename' ];
+            $urldata[ 'filename' ] = $path_parts[ 'filename' ];
+            $urldata[ 'extension' ] = $path_parts[ 'extension' ];
+            $urldata[ 'base' ] = $urldata[ 'scheme' ] . "://" . $urldata[ 'host' ];
+            $urldata[ 'abs' ] = ( isset($urldata[ 'path' ]) && strlen($urldata[ 'path' ]) ) ? $urldata[ 'path' ] : '/';
+            $urldata[ 'abs' ] .= ( isset($urldata[ 'query' ]) && strlen($urldata[ 'query' ]) ) ? '?' . $urldata[ 'query' ] : '';
+            //Set data
+            if ( $retdata ) {
+                return $urldata;
+            } else {
+                $this->urldata = $urldata;
+
+                return true;
+            }
+        } else {
+            //invalid URL
+            return false;
         }
+    }
+
+    public static function camelCase($str)
+    {
+        $str = self::convertToLowercase($str);
+
+        return preg_replace_callback('/_([a-z])/', function ($match) {
+            return strtoupper($match[ 1 ]);
+        }, $str);
+    }
+
+
+    public static function SentenceCase($string)
+    {
+        $sentences = preg_split('/([.?!]+)/', $string, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+        $newString = '';
+        foreach ( $sentences as $key => $sentence ) {
+            $newString .= ( $key & 1 ) === 0 ?
+                ucfirst(strtolower(trim($sentence))) :
+                $sentence . ' ';
+        }
+
         return trim($newString);
     }
-    
+
     /**
      * @param        $class
      * @param string $key
@@ -71,13 +116,13 @@ class UtilityManager
      *
      * @return bool
      */
-    public static function get_object_map( $class, $key = "ID", $field = "Title" )
+    public static function get_object_map($class, $key = "ID", $field = "Title")
     {
-        $oObjs       =  $class::get();
+        $oObjs = $class::get();
 
-        if ( count( $oObjs ) ) {
+        if ( count($oObjs) ) {
 
-            $objects_map  =  $oObjs->map( $key, $field );
+            $objects_map = $oObjs->map($key, $field);
 
             $aObjects = $objects_map->toArray();
 
@@ -86,91 +131,91 @@ class UtilityManager
 
         return [];
     }
-    
+
     /**
      * @param $class
      *
      * @return array
      */
-    public static function getObjectMap( $class )
+    public static function getObjectMap($class)
     {
         $oObjs = $class::get();
-        
-        return $oObjs ? $oObjs->map()->toArray() : array();
+
+        return $oObjs ? $oObjs->map()->toArray() : [];
     }
-    
+
     public static function getTemplateList()
     {
         $aTemplateList = [
             "Simple" => "List",
-        
+
         ];
-        
+
         return $aTemplateList;
     }
-    
-    public static function writeCssFile( $path, $css )
+
+    public static function writeCssFile($path, $css)
     {
-        
-        file_put_contents( $path, $css );
-        singleton( SiteTree::class )->flushCache();
-        
+
+        file_put_contents($path, $css);
+        singleton(SiteTree::class)->flushCache();
+
     }
-    
+
     /**
      * @param        $file
      * @param string $ext
      *
      * @return string
      */
-    private static function getCssFilePath( $file, $ext = 'css' )
+    private static function getCssFilePath($file, $ext = 'css')
     {
-        $path_parts = pathinfo( $file );
-        $path       = BASE_PATH . '/' . $path_parts[ 'dirname' ] . '/' . $path_parts[ 'filename' ] . '.' . $ext;
-        
+        $path_parts = pathinfo($file);
+        $path = BASE_PATH . '/' . $path_parts[ 'dirname' ] . '/' . $path_parts[ 'filename' ] . '.' . $ext;
+
         return $path;
     }
-    
+
     public static function HowDidYouHearAboutUsList()
     {
-        $list  = "Word of mouth,Google,Online Ads,Social Media";
-        $aList = explode( ',', $list );
-        
-        return array_combine( array_values( $aList ), array_values( $aList ) );
-        
+        $list = "Word of mouth,Google,Online Ads,Social Media";
+        $aList = explode(',', $list);
+
+        return array_combine(array_values($aList), array_values($aList));
+
     }
-    
+
     public static function CountyList()
     {
-        
+
         return static::getCountryList();
     }
-    
-    public static function getCountyNameFromCode( $code )
+
+    public static function getCountyNameFromCode($code)
     {
-        $code = strtoupper( $code );
-        
+        $code = strtoupper($code);
+
         $countryList = static::$aCountryList;
-        
+
         if ( !$countryList[ $code ] ) {
             return $countryList;
         } else {
             return $countryList[ $code ];
         }
     }
-    
+
     /**
      * @param $code
      *
      * @return array|mixed
      */
-    function code_to_country( $code )
+    function code_to_country($code)
     {
-        
-        return static::getCountyNameFromCode( $code );
+
+        return static::getCountyNameFromCode($code);
     }
-    
-    private static $aCountryList = array(
+
+    private static $aCountryList = [
         'AF' => 'Afghanistan',
         'AX' => 'Aland Islands',
         'AL' => 'Albania',
@@ -417,68 +462,68 @@ class UtilityManager
         'YE' => 'Yemen',
         'ZM' => 'Zambia',
         'ZW' => 'Zimbabwe',
-    );
-    
+    ];
+
     public static function getCountryList()
     {
         $aCountryList = static::$aCountryList;
-        
-        return array_combine( array_values( $aCountryList ), array_values( $aCountryList ) );
-        
+
+        return array_combine(array_values($aCountryList), array_values($aCountryList));
+
     }
-    
+
     /**
      * @param int $x
      * @param int $max
      *
      * @return array
      */
-    public static function getNumericValues( $x = 0, $max = 4 )
+    public static function getNumericValues($x = 0, $max = 4)
     {
-        $arrValues = array();
+        $arrValues = [];
         for ( $i = $x; $i <= $max; $i++ ) {
             $arrValues[ $i ] = $i;
         }
-        
+
         return $arrValues;
     }
-    
-    public static function FileExists( $oFile )
+
+    public static function FileExists($oFile)
     {
         $path = $oFile->getFullPath();
-        
-        return is_file( $path ) && file_exists( $path );
+
+        return is_file($path) && file_exists($path);
     }
-    
-    
+
+
     /**
      * @param string $name
      * @param string $title
      *
      * @return array
      */
-    public static function aMonths( $name = 'n', $title = 'F' )
+    public static function aMonths($name = 'n', $title = 'F')
     {
-        $months = array();
+        $months = [];
         for ( $m = 1; $m <= 12; $m++ ) {
-            $timestamp                           = mktime( 0, 0, 0, $m, 1 );
-            $months[ date( $name, $timestamp ) ] = date( $title, $timestamp );
+            $timestamp = mktime(0, 0, 0, $m, 1);
+            $months[ date($name, $timestamp) ] = date($title, $timestamp);
         }
-        
+
         return $months;
     }
-    
-    
+
+
     public static function aSalutationList()
     {
-        $list            = "Mr,Mrs,Ms,Miss,Dr";
-        $aSalutationList = explode( ',', $list );
-        
-        return array_combine( array_values( $aSalutationList ), array_values( $aSalutationList ) );
-        
+        $list = "Mr,Mrs,Ms,Miss,Dr";
+        $aSalutationList = explode(',', $list);
+
+        return array_combine(array_values($aSalutationList), array_values($aSalutationList));
+
     }
-    
-    
+
+
     /**
      * Create a global unique id
      *
@@ -486,10 +531,10 @@ class UtilityManager
      */
     public static function createGuid()
     {
-        if ( function_exists( 'com_create_guid' ) === true ) {
-            return trim( com_create_guid(), '{}' );
+        if ( function_exists('com_create_guid') === true ) {
+            return trim(com_create_guid(), '{}');
         }
-        
-        return strtolower( sprintf( '%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand( 0, 65535 ), mt_rand( 0, 65535 ), mt_rand( 0, 65535 ), mt_rand( 16384, 20479 ), mt_rand( 32768, 49151 ), mt_rand( 0, 65535 ), mt_rand( 0, 65535 ), mt_rand( 0, 65535 ) ) );
+
+        return strtolower(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535)));
     }
 }
